82f8fcd7bd557a0c1c256f524797b9beb359a519
Kirmanj Faris <kirmanj.00806510@gmail.com>
2018-08-23 07:46:22 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,8 +5,8 @@ function getRepositories() {
   }
 
   function showRepositories(event, data) {
-    //this is set to the XMLHttpRequest object that fired the event
-    var repos = JSON.parse(this.responseText)
+    
+    var repos = JSON.parse(this.responseText);
     console.log(repos)
     const repoList = `<ul>${repos.map(r => '<li>' + r.name + ' - <a href="#" data-repo="' + r.name + '" onclick="getCommits(this)">Get Commits</a></li>').join('')}</ul>`
     document.getElementById("repositories").innerHTML = repoList
