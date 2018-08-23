174fc48e5a4e97c7b8da65c5a0c63dde9a47acfc
Kirmanj Faris <kirmanj.00806510@gmail.com>
2018-08-23 08:33:23 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,8 +1,4 @@
-function getRepositories() {
-    const req = new XMLHttpRequest()
-    req.open("GET", 'https://api.github.com/users/octocat/repos/:owner/:repo/commits')
-    req.send()
-}
+
 
 function showRepositories(event, data) {
     
